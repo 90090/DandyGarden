17a0157e20a85b9e10ea884a3117c1db65482bbf
90090 <Ethan>
2022-10-15 12:46:24 -0400

    figured out how to change tabs

--- react-cext-boilerplate-main/src/popup/popupApp.jsx
+++ react-cext-boilerplate-main/src/popup/popupApp.jsx
@@ -1,9 +1,13 @@
+
 const PopupApp = () => {
   return (
 		<div style={{minWidth:'400px', minHeight:"400px"}}>
-			<h1 style={{ textAlign: "center" }}>This is Popup Script!!!</h1>
+			<button id="button" class="openGarden">Open</button>
 		</div>
   );
 };
+<script src="popup.js"></script>
+
+
 
 export default PopupApp;
