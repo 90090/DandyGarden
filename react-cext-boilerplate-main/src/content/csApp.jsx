fb142d069e48b7761f250435ff57d732d06052e7
90090 <Ethan>
2022-10-15 13:57:05 -0400

    delete

--- react-cext-boilerplate-main/src/content/csApp.jsx
+++
@@ -1,9 +0,0 @@
-const CSApp = () => {
-  return (
-		<div className="app-container">
-			 Live reload works!!
-		</div>
-	);
-};
-
-export default CSApp;
--- react-cext-boilerplate-main/src/options/optionsApp.js
+++
@@ -1,5 +0,0 @@
-const OptionsApp = () => {
-  return <h1 style={{ textAlign: "center" }}>Options Apps</h1>;
-};
-
-export default OptionsApp;
