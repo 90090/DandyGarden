c321499974c0e07c7716c422d6933410adf62ca8
rkb4472 <rkb4472>
2022-10-15 14:15:51 -0400

    started garden

--- react-cext-boilerplate-main/src/options/optionsApp.js
+++ react-cext-boilerplate-main/src/options/optionsApp.js
@@ -1,5 +1,23 @@
+import './options.css';
+
 const OptionsApp = () => {
-  return <h1 style={{ textAlign: "center" }}>Options Apps</h1>;
+  return (
+    <div className="ground">
+      <div className="top">
+        <div className="points">
+          <h1>POINTS: 25</h1>
+        </div>
+        <div className="dandygarden">
+          <h1>DandyGarden</h1>
+        </div>
+      </div>
+      <div className="points">
+          <h1>+ ADD PLANT</h1>
+        </div>
+    </div>
+
+
+    );
 };
 
 export default OptionsApp;
